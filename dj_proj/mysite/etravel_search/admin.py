d7ab040d356fc94eac24e8d96fc25c19f6a16796
ayushkul2910 <kulshrestha.ayush2910@gmail.com>
2018-02-19 17:49:26 +0530

    Cost table been added

--- dj_proj/mysite/etravel_search/admin.py
+++ dj_proj/mysite/etravel_search/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Bus,Search
+from .models import Bus,Search,Cost
 
 # Register your models here.
 
 admin.site.register(Bus,name='Bus')
 admin.site.register(Search,name='Search')
+admin.site.register(Cost,name='Cost')
