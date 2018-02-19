60fa4ac28489a6e5787579e3b0d258fd8ece61f9
ayushkul2910 <kulshrestha.ayush2910@gmail.com>
2018-02-19 17:47:03 +0530

    Etravel Service

--- dj_proj/mysite/etravel_search/models.py
+++ dj_proj/mysite/etravel_search/models.py
@@ -19,3 +19,12 @@ class Bus(models.Model):
 
     def __str__(self):
         return self.bus_name+' '+self.bus_type+' '+self.dep_time+' '+self.arr_time+' '+self.travel_time+' '+self.ratings+' '+self.seats+' '+self.price
+
+    
+class Cost(models.Model):
+    From_city=models.CharField(max_length=250)
+    To_city=models.CharField(max_length=250)
+    Price=models.CharField(max_length=6)
+
+    def __str__(self):
+        return str(self.From_city)+' '+str(self.To_city)+' '+str(self.Price)
