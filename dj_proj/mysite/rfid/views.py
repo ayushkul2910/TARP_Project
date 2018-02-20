b540b8b0a9223039fc6fd7ddc8269d81a65b1160
ayushkul2910 <kulshrestha.ayush2910@gmail.com>
2018-02-20 23:57:05 +0530

    continuous rfid touch

--- dj_proj/mysite/rfid/views.py
+++ dj_proj/mysite/rfid/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.template import loader
 from .models import Cost
 from etravel_search.models import User_Journey
 import serial
@@ -8,20 +9,19 @@ import serial
 
 
 def Ard(I):
-
-    
     ser=serial.Serial("COM3",9600)
     serin =ser.readline()
     serin=str(serin)[2:9]
     ser.close()
 
     if str(I)==str(serin):
-        return True
+        return True,serin
     else:
-        return False
+        return False,serin
 
 
 def index(request):
+    template=loader.get_template('index.html')
     J_ID=1 #different for every bus
     l=User_Journey.objects.all()
     S="Access Denied"
@@ -31,15 +31,20 @@ def index(request):
         U=record.User_id
         J=record.Journey_id
         if int(J)==J_ID:
-            flag=Ard(U)
+            flag,serin=Ard(U)
             if(flag):
                 S="Access Granted"
                 record.Session_id=True
                 record.save(update_fields=["Session_id"])
                 break
                 
+    context={
+        'S':S,
+        'U':serin,
+    }
 
-    return HttpResponse("<h2>"+S+"</h2>")
+
+    return HttpResponse(template.render(context,request))
 
 
 
